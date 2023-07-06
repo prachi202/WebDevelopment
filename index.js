939e3c2a5a92eafd891eeb60e5baf7de0940c8b3
prachi202 <94858301+prachi202@users.noreply.github.com>
2023-07-06 15:35:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,33 @@
-(function(){
-    console.clear();
-    "use strict";
-    function todolistManager() {
-        let addTask = document.querySelector(".cmp-todolistManager__taskManager__taskBox__addTask");
-        let tasks = document.querySelector(".cmp-todolistManager__taskManager__taskBox__tasks");
-        let clearAll = document.querySelector(".cmp-todolistManager__taskManager__messageBox--clear");
-        let message = document.querySelector(".cmp-todolistManager__taskManager__messageBox--span");
-        let searchForm = document.querySelector(".cmp-todolistManager__taskManager__searchBox__search");
-
-        function updateMessage() {
-            const taskLength = tasks.children.length;
-            message.textContent = `you have ${taskLength} pending tasks.`
-            
-        }
-        updateMessage();
-        addTask.addEventListener("submit", (event) => {
+(function() {
+    console.clear();        
+    "use strict";    
+    function quizApplication() {
+        const correctAnswers = ["A", "A"];
+        let quizForm = document.querySelector(".cmp-quizApplication__quizForm");
+        let result = document.querySelector(".cmp-quizApplication__result");
+        let yourScore = document.querySelector(".cmp-quizApplication__result--score");
+        let questions = document.querySelectorAll(".cmp-quizApplication__quizForm--question");
+        console.log(questions);
+        quizForm.addEventListener("submit", (event) => {
             event.preventDefault();
-            const value = addTask.task.value.trim();
-            if(value.length){
-                tasks.innerHTML += `<li class="cmp-todolistManager__taskManager__taskBox__tasks--list">
-                                        <span>${value}</span>
-                                        <i class="fa-solid fa-trash-can cmp-todolistManager__taskManager__taskBox__tasks--list--delete"></i>
-                                    </li>`;
-                addTask.reset();
-                updateMessage();
-            }
-        });
-
-        tasks.addEventListener("click", (event) => {
-            if(event.target.classList.contains("cmp-todolistManager__taskManager__taskBox__tasks--list--delete")){
-                event.target.parentElement.remove();
-                updateMessage();
-            }
-        });
-
-        clearAll.addEventListener("click", (event) => {
-            const taskItems = tasks.querySelectorAll("li");
-            taskItems.forEach(item => {
-                item.remove();
-            });
-            updateMessage()
-        });
-
-        function filterTask(searchTerm){
-             Array.from(tasks.children)
-            .filter((item) => {
-                return !item.textContent.toLowerCase().includes(searchTerm);
-            })
-            .forEach((item) => {
-                item.classList.add("cmp-todolistManager__taskManager__taskBox__tasks--list--hide");
-            })
-
-            Array.from(tasks.children)
-            .filter((item) => {
-                return item.textContent.toLowerCase().includes(searchTerm);
-            })
-            .forEach((item) => {
-                item.classList.remove("cmp-todolistManager__taskManager__taskBox__tasks--list--hide");
-            })
-        }
-
-        searchForm.addEventListener("keyup", (event) => {
-            const searchTerm = searchForm.task.value.trim().toLowerCase();
-            filterTask(searchTerm);
-        });
-
-        searchForm.addEventListener("click", (event) => {
-            if(event.target.classList.contains("cmp-todolistManager__taskManager__searchBox__search--reset")){
-                searchForm.reset();
-                const searchTerm = searchForm.task.value.trim();
-                filterTask(searchTerm);
+            let score = 0;
+            const userAnswers = [quizForm.q1.value, quizForm.q2.value];
+            userAnswers.forEach((answer, index) => {
+                if(answer === correctAnswers[index]){
+                    score = score + 1;
+                    questions[index].classList.add("cmp-quizApplication__quizForm--correct");
                 }
-        })
+                else{
+                    questions[index].classList.add("cmp-quizApplication__quizForm--wrong");
+                }
+            });
+            scrollTo(0,0);
+            result.classList.remove("cmp-quizApplication__hide");
+            //to select the first p tag of result using querySelector
+            // result.querySelector("p").textContent = `you scored ${score}/2`;
+            // yourScore.textContent = `you scored ${score}/2`;
+            yourScore.innerHTML = `you scored ${score}/2`;
+        });        
     }
-    todolistManager();
+    quizApplication();    
 }());
